47bed81df9c8820bb1b5f5312e5e05d3c017bf86
Patryk_Swierczek <patryk.swierczek@student.wsb.edu.pl>
2023-06-08 14:13:18 +0200

    delta

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Podaj współczynniki równania kwadratowego:");
+Console.WriteLine("A:");
+double a=double.Parse(Console.ReadLine());
+Console.WriteLine("B:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("C:");
+double c = double.Parse(Console.ReadLine());
+
+double dlt = b * b - 4 * a * c;
+Console.WriteLine(dlt);
